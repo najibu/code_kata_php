592f2250bdc941f063af6258ad18d7dda87f5e11
najibu <ns.najibu@gmail.com>
2017-02-06 13:51:33 +0000

    Added bowling game

---
+++ bowling-game/src/BowlingGame.php
@@ -0,0 +1,68 @@
+<?php
+
+class BowlingGame
+{
+  protected $rolls = [];
+
+  public function roll($pins)
+  {
+    $this->guardAgainstInvalidRoll($pins);
+
+    $this->rolls[] = $pins;
+  }
+
+  public function score()
+  {
+    $score = 0;
+    $roll = 0;
+
+    for($frame = 1; $frame <= 10; $frame++)
+    {
+      if ($this->isStrike($roll)) {
+        $score += 10 + $this->strikeBonus($roll);
+        $roll++;
+
+        continue;
+      }
+      if ($this->isSpare($roll)) {
+        $score += 10 + $this->spareBonus($roll);
+      } else {
+        $score += $this->getDefaultFrameScore($roll);       
+      }
+      $roll += 2;  
+    }
+    return $score;
+  }  
+
+  private function isSpare($roll)
+  {
+    return $this->rolls[$roll] + $this->rolls[$roll + 1] == 10;
+  }
+
+  private function getDefaultFrameScore($roll)
+  {
+    return $this->rolls[$roll] + $this->rolls[$roll + 1];;
+  }
+
+  private function isStrike($roll)
+  {
+    return $this->rolls[$roll] == 10;
+  }
+
+  private function strikeBonus($roll)
+  {
+    return $this->rolls[$roll + 1] + $this->rolls[$roll + 2];
+  }
+
+  private function spareBonus($roll)
+  {
+    return $this->rolls[$roll + 2];
+  }
+
+  private function guardAgainstInvalidRoll($pins)
+  {
+    If($pins < 0){
+      throw new InvalidArgumentException;
+    }
+  }
+}
